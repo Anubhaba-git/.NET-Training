05b0393e61e9453973f4230ebfab1129d152157c
Anubhaba Sahani <anubhabasahani87@gmail.com>
2024-10-29 22:15:42 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Number\tSquare\tCube");
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int square = i * i;
+            int cube = i * i * i;
+            Console.WriteLine($"{i}\t{square}\t{cube}");
+        }
+    }
+}
