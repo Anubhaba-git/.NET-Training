ce9c987c495633d59b1ec6e51260f6cb68fb60f8
Anubhaba Sahani <anubhabasahani87@gmail.com>
2024-10-29 23:35:50 +0530

    Print the even and odd numbers individually.

---
+++ Even and Odd numbers.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Even_and_Odd_numbers.cs;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nEven Numbers are:");
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Odd Numbers are:");
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
